6165f4ded44d21b87c77c406b44ede99a2b2b8f8
Silvia van Wingerden <silvia@jongensvantechniek.nl>
2019-05-03 17:47:14 +0200

    First setup search form wrapper

---
+++ components/search/searchFormWrapper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './searchFormWrapper.scss';
+
+export default class SearchFormWrapper extends Component {
+  render() {
+    return (
+      <div className="search-wrapper">
+        <h1>Water toys anytime anywhere</h1>
+        <h3>Discover the best water toys for your next adventure on the high seas.</h3>
+      </div>
+    );
+  }
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Default from '../../layouts/default';
 import './index.scss';
+import SearchFormWrapper from '../../components/search/searchFormWrapper';
 
 const meta = { title: 'Oceanpremium - Shop', description: 'Index description' };
 
 const IndexPage = () => (
   <Default meta={meta}>
     <div className="background-wrapper" />
+    <SearchFormWrapper />
   </Default>
 );
 
