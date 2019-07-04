6d1c232346312193e41340d03065806a497c741f
Peter Verkooijen <peter@peershaped.nl>
2019-07-04 16:06:57 +0200

    Make deliveryLocation always required

--- components/searchComponents/searchForm/searchValidation.js
+++ components/searchComponents/searchForm/searchValidation.js
@@ -3,6 +3,8 @@ import * as Yup from 'yup';
 const searchValidation = Yup.object().shape({
   // keyword: Yup.string()
   //   .required('This is a required field'),
+  deliveryLocation: Yup.string()
+    .required('This is a required field')
 });
 
 export default searchValidation;
