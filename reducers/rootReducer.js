dc5c32bae217d20422feb6a3df4cad780d6dcd35
Peter Verkooijen <peter@peershaped.nl>
2019-07-19 17:54:25 +0200

    Add localSearchReducer

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
 import cartReducer from './cartReducer';
 import locationReducer from './locationReducer';
+import localSearchReducer from './localSearchReducer';
 
 const rootReducer = combineReducers({
   searchReducer,
   cartReducer,
-  locationReducer
+  locationReducer,
+  localSearchReducer
 });
 
 export default rootReducer;
