9416f2322365ea53f872e4a7d00c3fb040d9d1af
Peter Verkooijen <peter@peershaped.nl>
2019-07-26 15:58:00 +0200

    Map orderrequest correctly

--- utils/mapping/products/orderRequest.js
+++ utils/mapping/products/orderRequest.js
@@ -7,34 +7,67 @@ export default class OrderRequest {
 
   returnOrderRequest() {
     const state = store.getState();
-
-    const obj = {
-      id: state.localSearchReducer.selectedProduct.id,
-      quantity: state.localSearchReducer.productQuantity,
-      period: {
-        start: state.localSearchReducer.search.deliveryDate,
-        end: state.localSearchReducer.search.collectionDate,
-      },
-      location: {
-        delivery: {
-          id: state.localSearchReducer.search.deliveryLocation.value.id,
-          name: state.localSearchReducer.search.deliveryLocation.value.name
+    let orderRequests = [];
+    orderRequests = state.cartReducer.items.map(item => {
+      const obj = {
+        id: item.selectedProduct.id,
+        quantity: item.productQuantity,
+        period: {
+          start: item.deliveryDate,
+          end: item.collectionDate,
         },
-        collection: {
-          id: state.localSearchReducer.search.collectionLocation.value.id,
-          name: state.localSearchReducer.search.collectionLocation.value.name
-        }
-      },
-    };
-    if (!isEmpty(state.localSearchReducer.productConfigurations)) {
-      this.configurations.map(item => ({ id: item.id, name: item.name, value: item.value }));
-      obj.configurations = this.configurations;
-    }
-    const list = state.localSearchReducer.productOptionalAccessories.filter(item => item.quantity !== 0);
-    if (!isEmpty(list)) {
-      list.map(item => ({ id: item.id, quantity: item.quantity }));
-      obj.accessories = list;
-    }
-    return obj;
+        location: {
+          delivery: {
+            id: item.deliveryLocation.value.id,
+            name: item.deliveryLocation.value.name
+          },
+          collection: {
+            id: item.collectionLocation.value.id,
+            name: item.collectionLocation.value.name
+          }
+        },
+      };
+      if (!isEmpty(item.productConfigurations)) {
+        this.configurations.map(configuration => ({ id: configuration.id, name: configuration.name, value: configuration.value }));
+        obj.configurations = this.configurations;
+      }
+      const list = item.productOptionalAccessories.filter(accessory => accessory.quantity !== 0);
+      if (!isEmpty(list)) {
+        list.map(accessory => ({ id: accessory.id, quantity: accessory.quantity }));
+        obj.accessories = list;
+      }
+      return obj;
+    });
+
+    console.log('roderRequests = ', orderRequests);
+    return orderRequests;
+    // const obj = {
+    //   id: state.localSearchReducer.selectedProduct.id,
+    //   quantity: state.localSearchReducer.productQuantity,
+    //   period: {
+    //     start: state.localSearchReducer.search.deliveryDate,
+    //     end: state.localSearchReducer.search.collectionDate,
+    //   },
+    //   location: {
+    //     delivery: {
+    //       id: state.localSearchReducer.search.deliveryLocation.value.id,
+    //       name: state.localSearchReducer.search.deliveryLocation.value.name
+    //     },
+    //     collection: {
+    //       id: state.localSearchReducer.search.collectionLocation.value.id,
+    //       name: state.localSearchReducer.search.collectionLocation.value.name
+    //     }
+    //   },
+    // };
+    // if (!isEmpty(state.localSearchReducer.productConfigurations)) {
+    //   this.configurations.map(item => ({ id: item.id, name: item.name, value: item.value }));
+    //   obj.configurations = this.configurations;
+    // }
+    // const list = state.localSearchReducer.productOptionalAccessories.filter(item => item.quantity !== 0);
+    // if (!isEmpty(list)) {
+    //   list.map(item => ({ id: item.id, quantity: item.quantity }));
+    //   obj.accessories = list;
+    // }
+    // return obj;
   }
 }
