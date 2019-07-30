91e8983371e89ed9b142ae05c5bc99c61b2ba6ba
Peter Verkooijen <peter@peershaped.nl>
2019-07-30 14:42:33 +0200

    Make method name plural

--- utils/mapping/products/orderRequest.js
+++ utils/mapping/products/orderRequest.js
@@ -5,7 +5,7 @@ import store from '../../../store';
 export default class OrderRequest {
   constructor() {}
 
-  returnOrderRequest() {
+  returnOrderRequests() {
     const state = store.getState();
     let orderRequests = [];
     orderRequests = state.cartReducer.items.map(item => {
@@ -39,35 +39,6 @@ export default class OrderRequest {
       return obj;
     });
 
-    console.log('roderRequests = ', orderRequests);
     return orderRequests;
-    // const obj = {
-    //   id: state.localSearchReducer.selectedProduct.id,
-    //   quantity: state.localSearchReducer.productQuantity,
-    //   period: {
-    //     start: state.localSearchReducer.search.deliveryDate,
-    //     end: state.localSearchReducer.search.collectionDate,
-    //   },
-    //   location: {
-    //     delivery: {
-    //       id: state.localSearchReducer.search.deliveryLocation.value.id,
-    //       name: state.localSearchReducer.search.deliveryLocation.value.name
-    //     },
-    //     collection: {
-    //       id: state.localSearchReducer.search.collectionLocation.value.id,
-    //       name: state.localSearchReducer.search.collectionLocation.value.name
-    //     }
-    //   },
-    // };
-    // if (!isEmpty(state.localSearchReducer.productConfigurations)) {
-    //   this.configurations.map(item => ({ id: item.id, name: item.name, value: item.value }));
-    //   obj.configurations = this.configurations;
-    // }
-    // const list = state.localSearchReducer.productOptionalAccessories.filter(item => item.quantity !== 0);
-    // if (!isEmpty(list)) {
-    //   list.map(item => ({ id: item.id, quantity: item.quantity }));
-    //   obj.accessories = list;
-    // }
-    // return obj;
   }
 }
