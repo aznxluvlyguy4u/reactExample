1d5783f229e0266ee62e37bbce5092c6d7a9d1a9
Peter Verkooijen <peter@peershaped.nl>
2019-10-11 12:47:59 +0200

    Handle 504 error

--- utils/rest/error/toastHandler.js
+++ utils/rest/error/toastHandler.js
@@ -3,9 +3,15 @@ import { toast } from 'react-toastify';
 export function handleGeneralError(error) {
   let string = 'An unkown error has occurred';
 
+
   if (error && error.code !== '404') {
     string = error.toString();
   }
+
+  if (error && error.code === '504') {
+    string = 'An unexpected error occured, Please refresh the page in a few seconds';
+  }
+
   toast.error(string, {
     position: toast.POSITION.TOP_RIGHT,
   });
